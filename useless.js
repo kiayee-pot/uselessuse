0749f3b26ae534dd6ad8dcff0da9d4474733eff7
kiayee-pot <kiayee@potatoproductions.com>
2025-12-07 15:39:53 +0800

    Add files via upload

---
+++ useless.js
@@ -0,0 +1,24 @@
+let crystal = document.getElementById("crystal");
+
+crystal.addEventListener("click", () => {
+    crystal.remove();
+});
+
+let stone = document.getElementById("stone");
+
+stone.addEventListener("click", () => {
+    stone.remove();
+});
+
+let grass = document.getElementById("grass");
+
+grass.addEventListener("click", () => {
+    grass.remove();
+});
+
+let spiral = document.getElementById("spiral");
+
+spiral.addEventListener("click", () => {
+    spiral.remove();
+});
+
