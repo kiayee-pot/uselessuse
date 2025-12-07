96d6708caec578dbb97006514a4267f0d81e8c88
kiayee-pot <kiayee@potatoproductions.com>
2025-12-07 21:08:11 +0800

    Add files via upload

---
+++ opencall.js
@@ -0,0 +1,12 @@
+let can = document.getElementById("can");
+
+can.addEventListener("click", () => {
+    can.remove();
+});
+
+
+let garbage = document.getElementById("garbage");
+
+garbage.addEventListener("click", () => {
+    garbage.remove();
+});
--- useless.js
+++ useless.js
@@ -22,3 +22,4 @@ spiral.addEventListener("click", () => {
     spiral.remove();
 });
 
+
